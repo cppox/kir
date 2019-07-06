486bc439778695fe8d077383435b945762ef4a27
farbod <farbodfb@icloud.com>
2019-07-06 07:30:12 +0430

    Update high.py

--- high.py
+++ high.py
@@ -3,12 +3,12 @@ from instapy import smart_run
 
 
 
-  session = InstaPy(username=high_ayatollah,
+session = InstaPy(username=high_ayatollah,
                   password=fertfert,
                   headless_browser=True)
 
 
 
-  with smart_run(session):
-    session.set_do_follow(enabled=True, percentage=10, times=2)
-    session.follow_user_followers(['parsalip', 'snoopdogg', 'wizkhalifa'], amount=400, randomize=False, sleep_delay=60)
+with smart_run(session):
+  session.set_do_follow(enabled=True, percentage=10, times=2)
+  session.follow_user_followers(['parsalip', 'snoopdogg', 'wizkhalifa'], amount=400, randomize=False, sleep_delay=60)
